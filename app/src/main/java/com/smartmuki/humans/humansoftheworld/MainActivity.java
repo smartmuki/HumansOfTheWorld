c82bb686265586c0e33073701d91692d7881b9a8
Mukesh Agarwal <smartmuki@gmail.com>
2015-07-21 18:25:33 +0530

    added facebook sdk

--- app/src/main/java/com/smartmuki/humans/humansoftheworld/MainActivity.java
+++ app/src/main/java/com/smartmuki/humans/humansoftheworld/MainActivity.java
@@ -1,16 +1,19 @@
 package com.smartmuki.humans.humansoftheworld;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.facebook.FacebookSdk;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
     }
 
